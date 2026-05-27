58489d9eaac3b8d49fb821f92f522805ea0051b2
LingyuCoder <lingyucoder@gmail.com>
2026-05-27 17:13:49 +0800

    fix(runtime): keep rspack runtime variables out of parser api

--- tests/rspack-test/configCases/runtime/runtime-variables-webpack-compat/index.js
+++ tests/rspack-test/configCases/runtime/runtime-variables-webpack-compat/index.js
@@ -3,7 +3,4 @@ const value = require("./dep");
 it("should run with rspack runtime variables", () => {
   expect(value).toBe(1);
   expect(DEFINED_DEP).toBe(1);
-  expect(typeof __rspack_require).toBe("function");
-  expect(typeof __rspack_modules).toBe("object");
-  expect(typeof __rspack_module.id).not.toBe("undefined");
 });
