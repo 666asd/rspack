011b522088fc4702a2ba72572c112bf4131ef8ca
LingyuCoder <lingyucoder@gmail.com>
2026-06-02 17:32:06 +0800

    fix: walk runtime helper assignment values

--- tests/rspack-test/configCases/runtime/runtime-mode-unsupported-require-property/errors.js
+++ tests/rspack-test/configCases/runtime/runtime-mode-unsupported-require-property/errors.js
@@ -23,4 +23,12 @@ module.exports = [
     message:
       /experiments\.runtimeMode: "rspack" does not support static __webpack_require__\.d helper property access/,
   },
+  {
+    message:
+      /experiments\.runtimeMode: "rspack" does not support static __webpack_require__\.d helper property access/,
+  },
+  {
+    message:
+      /experiments\.runtimeMode: "rspack" does not support static __webpack_require__\.t helper property access/,
+  },
 ];
--- tests/rspack-test/configCases/runtime/runtime-mode-unsupported-require-property/index.js
+++ tests/rspack-test/configCases/runtime/runtime-mode-unsupported-require-property/index.js
@@ -4,4 +4,5 @@ __webpack_require__.d = function () {};
 const boundGetter = __webpack_require__.d.bind(null);
 const nestedGetter = __webpack_require__.d.foo;
 __webpack_require__.d.foo = 1;
+__webpack_require__.d.bar = __webpack_require__.t;
 export { boundGetter, getter, nestedGetter };
